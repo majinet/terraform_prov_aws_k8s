f567d7435bb879d5b0c6471e6d1c1a1b6ae8fc28
majinet <majinet@gmail.com>
2023-02-26 11:36:18 +0800

    provisioning microk8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,16 +33,21 @@ module "aws_security_group" {
   vpc_id = module.aws_networks.vpc_id
 }
 
-resource "aws_instance" "control_plane" {
-  ami           = "ami-0b828c1c5ac3f13ee"
-  instance_type = "r5a.large"
+resource "aws_spot_instance_request" "control_plane" {
+  ami                    = "ami-0b828c1c5ac3f13ee"
+  spot_price             = 0.0732
+  instance_type          = "r5a.large"
+  spot_type              = "one-time"
+  # block_duration_minutes = 120
+  wait_for_fulfillment   = "true"
+
   subnet_id     = module.aws_networks.subnet_1_id
   associate_public_ip_address = true
   key_name = "terraform-ec2"
   security_groups = [module.aws_security_group.sg_1, module.aws_security_group.sg_microk8s]
 
   root_block_device {
-    volume_type = "gp2"
+    volume_type = "gp3"
     volume_size = 50
     delete_on_termination = true
   }
@@ -51,6 +56,25 @@ resource "aws_instance" "control_plane" {
     Name = "Kubernetes control plane instance"
   }
 }
+
+#resource "aws_instance" "control_plane" {
+#  ami           = "ami-0b828c1c5ac3f13ee"
+#  instance_type = "r5a.large"
+#  subnet_id     = module.aws_networks.subnet_1_id
+#  associate_public_ip_address = true
+#  key_name = "terraform-ec2"
+#  security_groups = [module.aws_security_group.sg_1, module.aws_security_group.sg_microk8s]
+
+#  root_block_device {
+#    volume_type = "gp2"
+#    volume_size = 50
+#    delete_on_termination = true
+#  }
+
+#  tags = {
+#    Name = "Kubernetes control plane instance"
+#  }
+#}
 
 #resource "aws_instance" "worker" {
 #  ami           = "ami-0b828c1c5ac3f13ee"  ami-0fa715233bba2f42e
