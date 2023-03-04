8ad7cca8c2af3eaab28652297bf5fcd9bb283613
majinet <majinet@gmail.com>
2023-03-04 08:47:45 +0800

    provisioning kubeflow

--- terraform/modules/aws-security-group/main.tf
+++ terraform/modules/aws-security-group/main.tf
@@ -27,6 +27,13 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
